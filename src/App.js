e357921f686cbb1138afe4d2734407d710f02a8e
merukode <auliarama965@gmail.com>
2024-08-05 20:12:49 +0700

    Update Homepage

--- src/App.js
+++ src/App.js
@@ -31,43 +31,49 @@ function App() {
     movie.title.toLowerCase().includes(search.toLowerCase())
   );
   return (
-    <main className="p-12">
+    <main className="bg-white">
       {/* Header */}
-      <div>
-        <h1>NontonFilms</h1>
-      </div>
-      {/* Input section */}
-      <section>
+      <div className="mb-10 flex flex-wrap justify-between bg-blue-800 p-10">
+        <h1 className="font-bold text-3xl text-white">AboutFilm</h1>
+        <section className="w-96">
         <input
-          class="w-full m-auto border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-[#ffe7c2] active:shadow-[2px_2px_0px_rgba(0,0,0,1)] rounded-md"
+          class="w-full mt-10 md:mt-0 border-black border-2 p-2.5 focus:outline-none focus:shadow-[2px_2px_0px_rgba(0,0,0,1)] focus:bg-[#ffe7c2] active:shadow-[2px_2px_0px_rgba(0,0,0,1)] rounded-md"
           placeholder="Cari Film"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
       </section>
+      </div>
+      {/* Main Section */}
+      <section className="p-10">
+        {/* Input section */}
+     
       {/* Popular Movies */}
-      <section className="mt-10 md:flex md:flex-wrap gap-10">
-        {filteredMovies.map((movie) => (
+      <h1 className="mt-10 mb-10 text-3xl font-bold">Popular Movies</h1>
+      <section className=" md:flex  md:p-5 scrollbar-bar hide-scrollbar md:overflow-x-scroll gap-10"> 
+        {filteredMovies.slice(0, 10).map((movie) => (
           <Link to={`/movie/${movie.id}`}>
-          <div class="w-80 h-full mb-5 border-black border-2 rounded-md hover:shadow-[8px_8px_0px_rgba(0,0,0,1)] bg-white">
+          <div class="w-full md:w-52 h-full mb-5 border-black border-2 rounded-md hover:shadow-[8px_8px_0px_rgba(0,0,0,1)] bg-white">
             <a href="" class="block cursor-pointer">
               <article class="w-full h-full">
                 <figure class="w-full h-1/2 border-black border-b-2">
                   <img
                     src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                     alt={movie.title}
-                    class="w-full h-full object-cover"
+                    class="w-full h-52 object-cover"
                   />
                 </figure>
                 <div class="px-6 py-5 text-left h-full">
-                  <p class="text-base mb-4">{movie.release_date}</p>
-                  <h1 class="text-[32px] mb-4">{movie.title}</h1>
+                  <p class="text-base mb-4 bg-blue-400 p-3 rounded-md w-fit">{movie.release_date}</p>
+                  <h1 class="text-xl font-bold">{movie.title}</h1>
+                  
                 </div>
               </article>
             </a>
           </div>
           </Link>
         ))}
+      </section>
       </section>
     </main>
   );
--- src/movie/MovieDetails.jsx
+++ src/movie/MovieDetails.jsx
@@ -16,6 +16,7 @@ const MovieDetails = () => {
             },
           }
         );
+        console.log("Bearer Token:", process.env);
 
         const data = await response.json();
         setMovie(data);
@@ -31,13 +32,26 @@ const MovieDetails = () => {
   }
 
   return (
-    <div className="p-12">
-      <h1>{movie.title}</h1>
-      <img
-        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-        alt={movie.title}
-      />
-    </div>
+    <section
+      className=" relative w-full h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
+      }}
+    >
+      <div className="absolute md:flex gap-5 p-10 inset-0 bg-black/70">
+        <img
+          className="border-black border-2 rounded-md"
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div className="md:flex text-white md:flex-col">
+          <p className="font-bold mt-3 text-gray-400">{movie.release_date}</p>
+          <h1 className="text-3xl mt-5 font-bold mb-5">{movie.title}</h1>
+          <p>{movie.overview}</p>
+          
+        </div>
+      </div>
+    </section>
   );
 };
 
